61d9f5ae2525e25a68e6aaa78ba977b5e112f6cc
franciscodalbertas <francisco.albertas@gmail.com>
2023-04-11 17:25:17 -0300

    baixando dados pecuaria pc local

--- scripts/pecuaria_IBGE.R
+++ scripts/pecuaria_IBGE.R
@@ -23,17 +23,12 @@ library(geobr)
 
 # abrindo municipios Cerrado
 
-cer_mun <- st_read(cer_mun,"/dados/projetos_andamento/TRADEhub/GLOBIOMbr/oc/mun_cerrado.shp")
+br_mun <- read_municipality(year="2020")
 
-# nao da pra baixar de mais 500 municipios, precisa dividir os municipios em grupos
 
-mun_df <- cer_mun[,4:5]
+num_groups = round(length(br_mun$code_mun)/300,0)
 
-# n of groups to stratify the access to the API
-
-num_groups = round(length(lista_mun)/500,0)
-
-list_df <- mun_df %>% 
+list_df <- br_mun %>% 
   group_by((row_number()-1) %/% (n()/num_groups)) %>%
   nest %>% pull(data)
 
@@ -55,48 +50,49 @@ info_sidra(74)
 
 
 # lista_mun <- unique(cer_mun$code_muni)
+
 tabela_leite <- 74
 geo <- "City"
 period <- 2017 # aqui como vou usar area pastagem do censo 2017, melhor pegar ano 2017 tb
 variavel <- c(106,215)
 classific <- c("c80","c80") #Grupos de área total(20):
-category  <-  list(2682,2684) # leite, la
+category  <-  list(2682) # leite, la
 
 # funcao pra baixar os dados
+
 f <- function(x,tabela,classificacao,categoria)get_sidra(tabela,geo=geo,geo.filter = list(x),variable = variavel,classific = classificacao,category = categoria)
 
 # rodando 
 # como aqui nao tem area de producao, acho q usar valor do mapbiomas de area de pastagem nao vai ficar bom.  
 
 # aqui nao baixou la
+
 leite_la <- lapply(mun_code,f,tabela=tabela_leite,classificacao=classific,categoria=category)
 
 # combining the data again
 
 leite_data_df <- as.data.frame(do.call(rbind,leite_la))
 
-
 #---- la --------------------------------------------------------------------
 # lista_mun <- unique(cer_mun$code_muni)
+
 tabela_leite <- 74
 geo <- "City"
 period <- 2017 # aqui como vou usar area pastagem do censo 2017, melhor pegar ano 2017 tb
 variavel <- c(106,215)
 classific <- c("c80") #Grupos de área total(20):
-category  <-  list(2684) #  la
+category_la  <-  list(2684) #  la
 
-# rodando 
+# rodando (faltou mudar codigo ano, area pastagem eh 2017, mas valor eh 2021)
 
 # aqui nao baixou la
-la <- lapply(mun_code,f,tabela=tabela_leite,classificacao=classific,categoria=category)
+la <- lapply(mun_code,f,tabela=tabela_leite,classificacao=classific,categoria=category_la)
 
 # combining the data again
 
 la_data_df <- as.data.frame(do.call(rbind,la))
 
 #---- carne --------------------------------------------------------------------
-
-
 
 # carne parece so ter no censo 2017 (precisaria dar um jeito de atualizar esse valor), ou usar dado do numero de cabeças, q tem pra 2021 -- mas dai precisa de valor da carcaca, bem mais dificil. Atualizar parece ideal
 
@@ -120,6 +116,8 @@ category  <-  list(111724,111725,111726,111727,111728,111729) # 111725,111726,11
 # 111728 Embutidos(linguiças, salsichas, etc.)
 # 111729 Couros e peles
 
+# loop pra produtos relacionados a carne
+
 carnes <- list()
 c <- 1
   for(cat in category)  {
@@ -135,18 +133,19 @@ carne_data_df <- as.data.frame(do.call(rbind,carnes))
 
 # compilando dados
 
-valor_venda <- carne_data_df %>%
+valor_venda_carne <- carne_data_df %>%
   filter(across(10)==10097)%>%
   group_by(across(c(1:4,6:7,9,11)))%>%
   summarise(valor_venda=sum(Valor,na.rm = T))
 
-valor_producao <- carne_data_df %>%
+valor_producao_carne <- carne_data_df %>%
   filter(across(10)==10096)%>%
   group_by(across(c(1:4,6:7,9,11)))%>%
   summarise(valor_producao=sum(Valor,na.rm = T))
 
+# ---- area de pastagem --------------------------------------------------------
 
-# acho q valor producao fica melhor. a questao agora eh qual area de pastagem usar. Mapbiomas??
+# acho q valor producao fica melhor. a questao agora eh qual area de pastagem usar. Mapbiomas?? (usei IBGE)
 
 # tabela 6878 tem área
 info_sidra(6878)
@@ -159,7 +158,7 @@ classific <- c("c829","c12517") #Grupos de área total(20):
 category  <-  list(46302,111523) # leite, la
 
 
-  x <- get_sidra(tabela_area_pastagem,geo=geo,geo.filter = mun_df$code_muni[1],variable = variavel,classific = classific,category = category)
+  # x <- get_sidra(tabela_area_pastagem,geo=geo,geo.filter = mun_df$code_muni[1],variable = variavel,classific = classific,category = category)
  
   
 area_pastagem <- lapply(mun_code,f,tabela=tabela_area_pastagem,classificacao=classific,categoria=category)
@@ -175,7 +174,7 @@ names(area_pastagem_df)[5] <- "area_pastagem_ha"
 
 # esses dados agregam carne vaca, suinos, embutidos, outros animais (ver comentario mais acima)
 
-valor_area_pastagem <- left_join(valor_producao,area_pastagem_df[,c(1,2,5,6,7)])
+valor_area_pastagem <- left_join(valor_producao_carne,area_pastagem_df[,c(1,2,5,6,7)])
 
 # valor/ha (reais)
 
@@ -183,7 +182,39 @@ valor_area_pastagem$reais_ha_carne <- (valor_area_pastagem$valor_producao*1000)/
 
 # na por 0
 
-valor_area_pastagem$reais_ha[is.na(valor_area_pastagem$reais_ha)] <- 0
+valor_area_pastagem$reais_ha_carne[is.na(valor_area_pastagem$reais_ha_carne)] <- 0
+
+
+# salvando as tabelas
+# 
+write.csv(valor_area_pastagem,"tabelas_IBGE/CENSO_2021_carne_rendimento_medio_ha.csv",row.names = F)
+
+# ---- completando dados de leite ----------------------------------------------
+
+
+valor_area_leite <- leite_data_df%>%
+  # so coluna valor producao
+  filter(across(10)==215)%>%
+  rename(valor_leite_reais=Valor)%>%
+  left_join(area_pastagem_df[,c(1,2,5,6,7)])%>%
+  mutate(reais_ha_leite=(valor_leite_reais*1000)/area_pastagem_ha)%>%
+  mutate(reais_ha_leite = replace_na(reais_ha_leite, 0))
+
+write.csv(valor_area_leite,"tabelas_IBGE/CENSO_2017_leite_rendimento_medio_ha.csv",row.names = F)
+
+#---- dados de la --------------------------------------------------------------
+  
+valor_area_la <- la_data_df%>%
+  # so coluna valor producao
+  filter(across(10)==215)%>%
+  rename(valor_la_reais=Valor)%>%
+  left_join(area_pastagem_df[,c(1,2,5,6,7)])%>%
+  mutate(reais_ha_la=(valor_la_reais*1000)/area_pastagem_ha)%>%
+  mutate(reais_ha_la = replace_na(reais_ha_la, 0))
+
+write.csv(valor_area_la,"tabelas_IBGE/CENSO_2021_la_rendimento_medio_ha.csv",row.names = F)
+
+#---- rasterizando dados -------------------------------------------------------
 
 # componente espacial
 
@@ -219,42 +250,6 @@ plot(carne_r)
 # salvando (pra cruzar com mapbiomas)
 raster::writeRaster(carne_r,"/dados/projetos_andamento/TRADEhub/GLOBIOMbr/oc/rendimento_medio_ha_carne_Mun_Cerrado_2017.tif",overwrite=TRUE)
 
-# ---- dados de leite ----------------------------------------------------------
-
-valor_area_leite <- leite_data_df%>%
-  # so coluna valor producao
-  filter(across(10)==215)%>%
-  rename(valor_leite_reais=Valor)%>%
-  left_join(area_pastagem_df[,c(1,2,5,6,7)])%>%
-  mutate(reais_ha_leite=(valor_leite_reais*1000)/area_pastagem_ha)%>%
-  mutate(reais_ha_leite = replace_na(reais_ha_leite, 0))
-  
-cer_mun_pj <- cer_mun_pj%>%
-  left_join(y = valor_area_leite[,c(6,15)],by=c("code_muni"="Município (Código)"))
-names(valor_area_leite)  
-
-leite_r <- fasterize(sf = cer_mun_pj,raster = r, field="reais_ha_leite",fun="first")
-
-# ajustar extent
-
-leite_r <- crop(leite_r,cer_pj)
-
-plot(leite_r)
-summary(leite_r[])
-
-# salvando (pra cruzar com mapbiomas)
-raster::writeRaster(leite_r,"/dados/projetos_andamento/TRADEhub/GLOBIOMbr/oc/rendimento_medio_ha_leite_Mun_Cerrado_2017.tif",overwrite=TRUE)
-
-
-#---- dados de la --------------------------------------------------------------
-  
-valor_area_la <- la_data_df%>%
-  # so coluna valor producao
-  filter(across(10)==215)%>%
-  rename(valor_la_reais=Valor)%>%
-  left_join(area_pastagem_df[,c(1,2,5,6,7)])%>%
-  mutate(reais_ha_la=(valor_la_reais*1000)/area_pastagem_ha)%>%
-  mutate(reais_ha_la = replace_na(reais_ha_la, 0))
 
 cer_mun_pj <- cer_mun_pj%>%
   left_join(y = valor_area_la[,c(6,15)],by=c("code_muni"="Município (Código)"))
@@ -270,3 +265,21 @@ summary(la_r[])
 
 # salvando (pra cruzar com mapbiomas)
 raster::writeRaster(la_r,"/dados/projetos_andamento/TRADEhub/GLOBIOMbr/oc/rendimento_medio_ha_la_Mun_Cerrado_2017.tif",overwrite=TRUE)
+
+
+cer_mun_pj <- cer_mun_pj%>%
+  left_join(y = valor_area_leite[,c(6,15)],by=c("code_muni"="Município (Código)"))
+names(valor_area_leite)  
+
+
+leite_r <- fasterize(sf = cer_mun_pj,raster = r, field="reais_ha_leite",fun="first")
+
+# ajustar extent
+
+leite_r <- crop(leite_r,cer_pj)
+
+plot(leite_r)
+summary(leite_r[])
+
+# salvando (pra cruzar com mapbiomas)
+raster::writeRaster(leite_r,"/dados/projetos_andamento/TRADEhub/GLOBIOMbr/oc/rendimento_medio_ha_leite_Mun_Cerrado_2017.tif",overwrite=TRUE)
