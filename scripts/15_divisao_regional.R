a684a2b835ca8f6288eefa84f84462aada1bc99d
franciscodalbertas <francisco.albertas@gmail.com>
2023-09-12 17:19:06 -0300

    rodando os modelos com as novas variáveis

--- scripts/15_divisao_regional.R
+++ scripts/15_divisao_regional.R
@@ -6,7 +6,7 @@
 
 # pacotes ----------------------------------------------------------------------
 
-library(geobr)
+#library(geobr)
 library(data.table)
 library(dplyr)
 library(sf)
@@ -14,44 +14,47 @@ library(sf)
 #-------------------------------------------------------------------------------
 
 # regioes do BR:
-
-#reg <- geobr::read_region()
-
-mun <- read_municipality(year = "2019") # 5
-meso <- read_meso_region(year = "2019") # 139
-inter_read <- read_intermediate_region()# 135
-regioes_rurais <- st_read("/dados/projetos_andamento/custo_oportunidade/shapes/RR_Regioes_Rurais2015.shp")
+# resolvi seguir com 5 regioes
+mun <- st_read("/dados/projetos_andamento/custo_oportunidade/mun_data/Brazil_municipalities_2020_mollenweide.shp") # 5
+# meso <- read_meso_region(year = "2019") # 139
+# inter_read <- read_intermediate_region()# 135
+# regioes_rurais <- st_read("/dados/projetos_andamento/custo_oportunidade/shapes/RR_Regioes_Rurais2015.shp")
 
 # ---- macroregiões ------------------------------------------------------------
 
 
 # simplificando
-
-reg <- mun %>% distinct(abbrev_state, name_region, code_region)
-
-# # drop geo
 # 
-# st_geometry(mun) <- NULL
+# reg <- mun %>% distinct(abbrv_s, nam_rgn, cod_rgn)
 
 # dados brutos entrada
 
 
-df <- fread("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/independent_variables_complete_updated.csv")
+df <- fread("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/independent_variables_updated_regreen.csv")
 
-# adicionando info regiao (esta adicionando linhas)
+# eliminando parte espacial mun
 
-df2 <- left_join(reg,df
-                 #, by = join_by(code_muni == code_muni_IBGE)
-                 )
+st_geometry(mun) <- NULL
+
+# adicionando UF e regiao
+
+df2 <- left_join(df,mun,join_by(code_muni_IBGE == code_mn))
+
+
+# # adicionando info regiao (esta adicionando linhas)
+# 
+# df2 <- left_join(reg,df
+#                  #, by = join_by(code_muni == code_muni_IBGE)
+#                  )
 
 # Split the data frame 
 
-split_df <- df2 %>% group_split(code_region)
+split_df <- df2 %>% group_split(cod_rgn)
 
 # Save the data frames to separate .csv files
 
 lapply(split_df, function(df) {
-  filename <- paste0("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/regioes_5/", df$code_region[1], ".csv")
+  filename <- paste0("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/regioes_5/", df$nam_rgn[1], ".csv")
   write.csv(df, filename, row.names = FALSE)
 })
 
--- scripts/18_adjusting_model_regional.R
+++ scripts/18_adjusting_model_regional.R
@@ -6,16 +6,17 @@
 
 # pacotes ----------------------------------------------------------------------
 
-library(data.table)
+library(data.table) # abre dfs grandes
 library(dplyr)
 #library(sf)
 #library(tidyr)
 #library(purrr)
-library(sampler)
+library(sampler) # amostragem estratificada
 library(ggcorrplot)
 library(randomForestSRC)
 library(ggRandomForests)
-library(randomForest)
+#library(randomForest)
+# esses sao pra avaliacao modelo
 library(DHARMa)
 library(Boruta)
 
@@ -29,32 +30,38 @@ p <- "/dados/projetos_andamento/custo_oportunidade/data_econometric_model/regioe
 
 f <- list.files(p,full.names = T) 
 
-# variaveis preditoras
+# variaveis preditoras (rever, ta errado as posicoes a 4 eg sudeste mas tinha q ser Sul)
 
-reg_4 <- fread(f[4])
-reg_3 <- fread(f[3])
-reg_2 <- fread(f[2])
-reg_5 <- fread(f[5])
-reg_1 <- fread(f[1])
+reg_3 <- fread(f[4]) # sudeste
+reg_1 <- fread(f[3]) # Norte
+reg_2 <- fread(f[2]) # Nordeste
+reg_4 <- fread(f[5]) # Sul
+reg_5 <- fread(f[1]) # C-O
+
+# as variaveis agressoes e conflitos so existem pra Amazonia, entao precisa tirar das outras regioes.
+
+reg_3 <- subset(reg_3, select = -c(agressoes, conflitos))
+reg_2 <- subset(reg_2, select = -c(agressoes, conflitos))
+reg_4 <- subset(reg_4, select = -c(agressoes, conflitos))
+reg_2 <- subset(reg_2, select = -c(agressoes, conflitos))
 
 # df com variavel resposta:
 
-vr <- fread("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/full_dataset_complete_cases_new_variables.csv")
+vr <- fread("/dados/projetos_andamento/custo_oportunidade/data_econometric_model/response_updated_regreen.csv")
 
-# join com cod mun, x e y + VTN
+# arredondando x e y pra bater o join
 
-# x= 10
-# y =11
-# VTN = 2
-# cod mun = 3
-# abbrev_state = 16
+vr$x <- round(vr$x)
+vr$y <- round(vr$y)
+
+# join com  x e y + VTN 
 
 # deixar generico pra valer pra todas (pode ser um loop)
 
-reg <- reg_5 # muda dependendo da regiao interesse
+reg <- reg_3 # muda dependendo da regiao interesse
 
 
-reg <- left_join(reg,vr[,c(2,3,10,11,17)])
+reg <- left_join(reg,vr)
 
 # pacote sampler
 
@@ -83,26 +90,29 @@ test_sc <- testData
 # codigo regiao e municipio tem que ser fator
 
 train_sc$code_muni_IBGE <- as.factor(train_sc$code_muni_IBGE)
-train_sc$code_region <- as.factor(train_sc$code_region)
+train_sc$cod_rgn <- as.factor(train_sc$cod_rgn)
+train_sc$cod_stt <- as.factor(train_sc$cod_stt)
 
 test_sc$code_muni_IBGE <- as.factor(test_sc$code_muni_IBGE)
-test_sc$code_region <- as.factor(test_sc$code_region)
+test_sc$cod_rgn <- as.factor(test_sc$cod_rgn)
 test_sc$abbrev_state <- as.factor(test_sc$abbrev_state)
-
+test_sc$cod_stt <- as.factor(test_sc$cod_stt)
 
 # aplicando scale pras variaveis continuas (inclui x e y como variaveis escaladas)
 
 # Select the continuous variables. can't indluce VTN
 
-continuous_variables <-which(sapply(train_sc, is.numeric) & !names(train_sc) == "VTN_2022")
+# apagar, nao ta mais funcionando
 
-continuous_variables_test <-which(sapply(test_sc, is.numeric) & !names(test_sc) == "VTN_2022")
+continuous_variables <- which(sapply(train_sc, is.numeric) & !names(train_sc) == "vtn")
+
+continuous_variables_test <-as.numeric(which(sapply(test_sc, is.numeric) & !names(test_sc) == "vtn"))
 
 # scale them
 
-train_sc_continuous <- as.data.frame(apply(train_sc[,c(continuous_variables)],2,scale))
+train_sc_continuous <- as.data.frame(apply(subset(train_sc, select = names(continuous_variables)),2,scale))
 
-test_sc_continuous <- as.data.frame(apply(test_sc[,c(continuous_variables_test)],2,scale))
+test_sc_continuous <- as.data.frame(apply(subset(test_sc, select = names(continuous_variables)),2,scale))
 
 
 # Create a new data frame with the scaled continuous variables and the non-continuous variables
@@ -110,10 +120,9 @@ test_sc_continuous <- as.data.frame(apply(test_sc[,c(continuous_variables_test)]
 train_sc <- cbind(train_sc[, -continuous_variables], train_sc_continuous)
 test_sc <- cbind(test_sc[, -continuous_variables], test_sc_continuous)
 
-
 # Create a scatterplot matrix
 
-cor_df <- train_sc[,continuous_variables]
+cor_df <- train_sc_continuous
 cor_mat <- cor(cor_df)
 
 # tem variaveis correlacionadas
@@ -123,16 +132,26 @@ cor_mat <- cor(cor_df)
 corplot <- ggcorrplot(cor_mat, hc.order = TRUE, type = "lower",
            color = "blue", outline.color = "black",digits = 3)
 
-# variaveis correlacionadas:
-
-# regiao 4:
-# x e Distancia cidades e portos (>0.7)
+# variaveis correlacionadas (>0.7):
+#******************************************************
+# regiao 4 (Sul):
+#******************************************************
+# x com Distancia  portos, dist Garimpo e dist. cidades
+# Dist cidades vs dist garimpo
 # prop.agri e prop. nat veg (>0.7)
 
-# regiao 3:
-# clima e distancia cidades (WTF?!!)
+# manter x e prop. agri
 
+#******************************************************
+# regiao 3 (Sudeste):
+#******************************************************
+# clima e distancia cidades (WTF?!!)
+# distancia cidades e y
+
+
+#******************************************************
 # regiao 2:
+#******************************************************
 # clima e x 
 # propnat veg e prop agri
 
@@ -146,14 +165,26 @@ corplot <- ggcorrplot(cor_mat, hc.order = TRUE, type = "lower",
 
 # ajustando modelo inicial (FULL)
 
-# variaveis preditoras sem dist cidades e portos e sem prop. nat. veg.
+# variaveis preditoras excluindo var. correlacionadas
 
-# regiao 4:
+for(regiao in 1:5){
 
-# excluir <- c("PropNatVeg","VTN_2022","dist_portos","DistCitiesover500k","VTN_2022_log","code_muni_IBGE","name_region","code_region")
+  if(unique(reg$cod_rgn)==regiao){
+  
+  # regiao 4:
+  
+    excluir <- c("PropNatVeg","vtn","dist_portos","DistCitiesover500k","code_muni_IBGE","nam_rgn","cod_rgn","DistGarimp","name_mn","cod_stt","abbrv_s","abbrev_state","nam_stt")
+  }
 
-# regiao 3:
-# eliminei climate
+  if(unique(reg$cod_rgn)==regiao){
+    
+    excluir <- c("vtn","code_muni_IBGE","nam_rgn","cod_rgn","name_mn","cod_stt","abbrv_s","abbrev_state","nam_stt","DistCitiesover500k")  
+  }
+  
+
+# terminar de incluir as outras regioes no loop
+
+
 
 #excluir <- c("VTN_2022","VTN_2022_log","code_muni_IBGE","name_region","code_region","Climate")
 
@@ -171,14 +202,15 @@ corplot <- ggcorrplot(cor_mat, hc.order = TRUE, type = "lower",
 
 #excluir <- c("PropNatVeg","VTN_2022","VTN_2022_log","code_muni_IBGE","name_region","code_region")
 
+}
 
 # corrigindo nomes - tira % prop com energia 
 
-names(train_sc)[24] <- "prop_com_energia"
-names(train_sc)[25] <- "prop_com_ens_superior"
+names(train_sc)[28] <- "prop_com_energia"
+names(train_sc)[29] <- "prop_com_ens_superior"
 
-names(test_sc)[24] <- "prop_com_energia"
-names(test_sc)[25] <- "prop_com_ens_superior"
+names(test_sc)[28] <- "prop_com_energia"
+names(test_sc)[29] <- "prop_com_ens_superior"
 
 # excluindo variaveis correlacionadas e resposta
 
@@ -186,15 +218,15 @@ pred_posicao <- unlist(which(!names(train_sc) %in% excluir))
 pred_varnames <- names(train_sc[,pred_posicao])
 
 
-formula_full <- as.formula(paste("VTN_2022_log ~", paste(pred_varnames, collapse = "+")))
+formula_full <- as.formula(paste("VTN_log ~", paste(pred_varnames, collapse = "+")))
 
 # adicionando var. resp. em log
 
-train_sc$VTN_2022_log <- log(train_sc$VTN_2022)
+train_sc$VTN_log <- log(train_sc$vtn)
 
-hist(train_sc$VTN_2022)
-hist(train_sc$VTN_2022_log)
-summary(train_sc$VTN_2022_log)
+hist(train_sc$VTN_log)
+hist(train_sc$vtn)
+summary(train_sc$VTN_log)
 
 # Convert all character columns to factors
 
@@ -206,21 +238,23 @@ set.seed(111)
 
 boruta.train <- Boruta(formula_full, data = train_sc, doTrace = 2)
 
-
 print(boruta.train)
 
 plot(boruta.train, cex.axis = 0.8)
 
 # pra calcular erro por arvore precisa da opcao block.size=1! 
 
-rfModel_full <- rfsrc(formula = formula_full , data = as.data.frame(train_sc), ntree = 200,nodesize = 20,block.size = 1)
+rfModel_full <- rfsrc(formula = formula_full , data = as.data.frame(train_sc), ntree = 400,nodesize = 20,block.size = 1)
 
 summary(rfModel_full)
 
+# testar retirar variaveis
+#updated_model <- update(rfModel_full, . ~ . - PropPrivada - IDHm2010)
 
 # rfModel_lerdo <- randomForest(formula = formula_full  , data = train_sc, ntree = 100,nodesize = 20) # discutir node size mas com n grande eh ok
 
 plot(rfModel_full)
+plot(updated_model)
 
 erro <- gg_error(rfModel_full, error.type = "oob")
 
@@ -230,21 +264,28 @@ plot(erro)
 #plot(gg_variable(rfModel_full))
 
 plot(gg_vimp(rfModel_full))
+plot(gg_vimp(updated_model))
 
 # acuraria:
 
-actual <- log(test_sc$VTN_2022)
-test_sc$VTN_2022_log <- log(test_sc$VTN_2022)
+actual <- log(test_sc$vtn)
+test_sc$VTN_log <- log(test_sc$vtn)
 # mantendo apenas as mesmas colunas!
 test_sc2 <- as.data.frame(test_sc)
 #test_sc2 <- test_sc %>% select(names(train_sc))
 predicted <- predict(object = rfModel_full, newdata = test_sc)
-r_full <- caret::R2(pred = predicted$predicted,obs = actual) # 0.95 (r4),0.97 (r3).0.92 (2), 0.96(5)/ 0.94(1)!!
+predicted_up <- predict(object = updated_model, newdata = test_sc)
+r_full <- caret::R2(pred = predicted$predicted,obs = actual) # 0.89 (r4),0.96 (r3).0.92 (2), 0.96(5)/ 0.94(1)!!
+r_up <- caret::R2(pred = predicted_up$predicted,obs = actual)
 r_rmse <- caret::RMSE(pred = predicted$predicted,obs = actual)#1:0.19;2:0.27;3=0.23; 4=1.60; 5=0.19
 
-library(ggpubr)
+# apagando objetos pra rodar outra regiao
 
-toplot <- data.frame(atual=actual,predicted=predicted$predicted)
+rm(rfModel_full,s,test_sc,test_sc_continuous,test_sc,testData,train_sc,trainData,updated_model,boruta.train,cor_df,cor_mat,corplot,erro,predicted,predicted_up,reg,train_sc_continuous,test_sc2,actual,continuous_variables,continuous_variables_test,excluir,f,formula_full,fracao_amostragrem,pred_posicao,pred_varnames,r_full,r_rmse,r_up,trainIndex)
 
-ggscatter(x = "atual",y="predicted",data = toplot)
+# library(ggpubr)
+# 
+# toplot <- data.frame(atual=actual,predicted=predicted$predicted)
+# 
+# ggscatter(x = "atual",y="predicted",data = toplot)
 
